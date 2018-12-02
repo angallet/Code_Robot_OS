76a207313430edd085409dc58432d80461bfebc1
Paul Monniot <paul.monniot@telecom-paristech.fr>
2018-12-02 16:38:22 +0100

    fixes typos in main.c and comments libbluetooth in makefile

--- source/main.c
+++ source/main.c
@@ -3,6 +3,7 @@
 #include "ev3.h"
 #include "ev3_port.h"
 #include "ev3_sensor.h"
+#include "ev3_tacho.h"
 // WIN32 /////////////////////////////////////////
 #ifdef __WIN32__
 #include <windows.h>
@@ -25,23 +26,23 @@ int main( void )
 {
     char s[ 256 ];
     int val;
-    uint8_t snA,snD;
+    uint8_t snA,snD, sn_touch;
     FLAGS_T state;
     uint32_t n, i, ii;
     printf( "Waiting the EV3 brick online...\n" );
     if ( ev3_init() < 1 ) return ( 1 );
-    whille ( ev3_tacho_init() < 1 ) Sleep( 1000 );
+    while ( ev3_tacho_init() < 1 ) Sleep( 1000 );
     printf( "*** ( EV3 ) Hello! ***\n" );
     ev3_sensor_init();
     printf( "Found sensors:\n" );
-    if ( eev3_search_tacho( LEGO_EV3_M_MOTOR, &snA, 0 ) && eev3_search_tacho( LEGO_EV3_M_MOTOR, &snD, 0 )) {
+    if ( ev3_search_tacho( LEGO_EV3_M_MOTOR, &snA, 0 ) && ev3_search_tacho( LEGO_EV3_M_MOTOR, &snD, 0 )) {
         int max_speed;
         printf( "LEGO_EV3_M_MOTOR is found, run for 5 sec...\n" );
         get_tacho_max_speed( snA, &max_speed );
         get_tacho_max_speed( snD, &max_speed );
         printf("  max_speed = %d\n", max_speed );
-        set_tacho_stop_action_inx( snA, TACHO_COAST );
-        set_tacho_stop_action_inx( snD, TACHO_COAST );
+        set_tacho_stop_action_inx( snA, TACHO_COAST);
+        set_tacho_stop_action_inx( snD, TACHO_COAST);
         set_tacho_speed_sp( snA, max_speed * 2 / 3 );
         set_tacho_speed_sp( snD, max_speed * 2 / 3 );
         set_tacho_time_sp( snA, 5000 );
