b1592dafe56a5c6bc01a38ed2e07fcb39f9073b1
Antoine G <gallet@eurecom.fr>
2018-12-11 16:56:54 +0100

    bluetooth messages receptions

--- source/bluetooth.c
+++ source/bluetooth.c
@@ -129,34 +129,6 @@ int init_bluetooth( void )
         return ( 1 );
 }
 
-{
-        struct sockaddr_rc addr = { 0 };
-        int status;
-        //struct timeval timeout;
-        //timeout.tv_sec = READ_TIMEOUT_SEC;
-        //timeout.tv_usec = 0;
-
-        // allocate a socket
-        s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
-        //setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, (const char*)&timeout,sizeof(struct timeval));    // Set a timeout
-
-        // set the connection parameters (who to connect to)
-        addr.rc_family = AF_BLUETOOTH;
-        addr.rc_channel = (uint8_t) 1;
-        str2ba(SERV_ADDR, &addr.rc_bdaddr);
-
-        // connect to server
-        status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
-
-        // if not connected
-        if( status != 0 ) {
-                print_error("Failed to connect to server...");
-                bluetooth_state = DISCONNECTED;
-                return ( 1 ); // TODO change to 0 when server is available
-        }
-        bluetooth_state = CONNECTED;
-        return ( 1 );
-}
 
 
 void *bluetooth_main(void *arg) {
