953a4f1d2da8b6846c90b77345ff6a25010bf2a8
Arthur Dion <arthur.dion@telecom-paristech.fr>
2018-12-15 16:45:45 +0100

    Again change the value to tune the system

--- source/motors.c
+++ source/motors.c
@@ -59,10 +59,10 @@ void turn (int degree)
       uint8_t sn;
       int port=65;
       int time;
-      time = 6*degree; // 540/180 : 540 time
-      sg_motor(port, time, 300);
+      time = 12*degree; // 540/180 : 540 time
+      sg_motor(port, time, 150);
       port=68;
-      sg_motor(port, time, -300);
+      sg_motor(port, time, -150);
 }
 
 
