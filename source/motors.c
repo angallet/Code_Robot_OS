754b5f0f267d0bd737393489fe1ad5e31b6eee3b
Arthur Dion <dion@eurecom.fr>
2019-01-09 15:35:53 +0100

    adding comment

--- source/motors.c
+++ source/motors.c
@@ -16,35 +16,17 @@
 
 #include <unistd.h>
 #define Sleep( msec ) usleep(( msec ) * 1000 )
-#define MOTOR_LEFT     OUTD
-#define MOTOR_RIGHT    OUTA
-#define MOTOR_BOTH     ( MOTOR_LEFT | MOTOR_RIGHT )
 #define M_PI 3.14159265358979323846
 #define SPEED 250
 //////////////////////////////////////////////////
 #endif
 
-
-/*
-void quarter_turn(void)
-{
-  int port=65;
-    if ( ev3_search_tacho_plugged_in(port,0, &sn, 0 )) {
-      int max_speed;
-      get_tacho_max_speed( sn, &max_speed );
-      set_tacho_stop_action_inx( sn, TACHO_COAST );
-      set_tacho_speed_sp( sn, max_speed/5);
-}
-*/
-//126cm/10s
-//0.126m/s
-//12,6cm/s
-//distance in centimeter
-
+// function to move forward a set distance given by the parameters distance
 void move_forward (int distance)
 {
   uint8_t sn;
   int port=65;
+  // computation of the time necessary to move of the corresponding distance
   int time= (int)(distance/12.6)*1000;
   printf("the robot will run during : %d\n",time);
   sg_motor(port, time, SPEED);
@@ -53,10 +35,12 @@ void move_forward (int distance)
   Sleep(time);
 }
 
+// function to move backward a set distance given by the parameters distance
 void move_backward (int distance)
 {
   uint8_t sn;
   int port=65;
+  // computation of the time necessary to move of the corresponding distance
   int time= (int)(distance/12.6)*1000;
   printf("the robot will run during : %d\n",time);
   sg_motor(port, time, -SPEED);
@@ -65,6 +49,7 @@ void move_backward (int distance)
   Sleep(time);
 }
 
+// function to turn right using the gyroscope
 void turn_gyro_right(int degree)
 {
   uint8_t sn_gyro;
@@ -99,6 +84,7 @@ void turn_gyro_right(int degree)
   }
 }
 
+// function to turn left
 void turn_left (int degree)
 {
       //printf("enter into the function turn\n");
@@ -112,7 +98,7 @@ void turn_left (int degree)
       Sleep(time);
 }
 
-
+// function to turn right
 void turn_right (int degree)
 {
       //printf("enter into the function turn\n");
@@ -126,24 +112,12 @@ void turn_right (int degree)
       Sleep(time);
 }
 
-
-void sg_motor_deg (int port, int deg, int speed)
-{
-    uint8_t sn;
-    if ( ev3_search_tacho_plugged_in(port,0, &sn, 0 )) {
-      set_tacho_stop_action_inx( sn, TACHO_COAST );
-      set_tacho_position_sp(sn, deg);
-      set_tacho_speed_sp( sn, speed);
-      set_tacho_command_inx(sn, TACHO_RUN_TO_REL_POS);
-    }
-}
-
+// function to run a single motor during a given time at a given speed
 void sg_motor (int port, int time, int speed)
 {
     uint8_t sn;
     if ( ev3_search_tacho_plugged_in(port,0, &sn, 0 )) {
-      //printf( "LEGO_EV3_M_MOTOR 1 is found, run...\n" );
-      //printf("  speed = %d\n", speed );
+      //printf("Motor on port %d run at speed = %d\n", port, speed );
       set_tacho_stop_action_inx( sn, TACHO_COAST );
       set_tacho_speed_sp( sn, speed);
       set_tacho_time_sp( sn, time );
@@ -151,25 +125,7 @@ void sg_motor (int port, int time, int speed)
     }
 }
 
-
-
-
-void init_motors(void)
-{
-    if (tacho_is_plugged(MOTOR_BOTH, TACHO_TYPE__NONE_))
-    {
-        tacho_reset(MOTOR_BOTH);
-        set_tacho_stop_action_inx(MOTOR_BOTH, TACHO_COAST);
-        printf("Motors found\n");
-    }
-    else
-    {
-        printf("Check the connection of the motor, ");
-        printf("unable to establish connection");
-    }
-}
-
-
+// funcion to turn a quarter turn on the left
 void quarter_turn (void)
 {
   uint8_t sn;
@@ -179,27 +135,34 @@ void quarter_turn (void)
   sg_motor(port, 540, -300);
 }
 
+// function to permform a 3 point throw
 void throw (void)
 {
   uint8_t sn;
   int port=67;
   if ( ev3_search_tacho_plugged_in(port,0, &sn, 0 )) {
+    // set the position
     set_tacho_position_sp(sn, 140);
+    // set the speed
     set_tacho_speed_sp( sn, 1049);
+    // initiate the command with the position and speed given previously
     set_tacho_command_inx(sn, TACHO_RUN_TO_REL_POS);
+
     Sleep(500);
+
     set_tacho_position_sp(sn, -30);
     set_tacho_command_inx(sn, TACHO_RUN_TO_REL_POS);
     set_tacho_stop_action_inx( sn, TACHO_COAST );
   }
 }
 
+// function to disable catapult
 void disable_catapult(void)
 {
     uint8_t sn;
     int port=67;
     if ( ev3_search_tacho_plugged_in(port,0, &sn, 0 )) {
-        printf("disable_catapult\n");
+      printf("disable_catapult\n");
 
       set_tacho_position_sp(sn, 140);
       set_tacho_speed_sp( sn, 500);
@@ -209,6 +172,7 @@ void disable_catapult(void)
     }
 }
 
+// function to enable the catapult 
 void enable_catapult(void)
 {
     uint8_t sn;
@@ -220,41 +184,37 @@ void enable_catapult(void)
       set_tacho_speed_sp( sn, 500);
       set_tacho_command_inx(sn, TACHO_RUN_TO_REL_POS);
       set_tacho_stop_action_inx( sn, TACHO_COAST );
+
       Sleep(1000);
 
 
     }
 }
 
+// function to get the ball
 void get_ball(int move_value)
 {
+    int motor_lift = 66;
+
     printf( "%d this is the color\n", val);
     fflush( stdout );
-    int motor_lift = 66;
     printf("get_ball\n" );
+
     disable_catapult();
+
     move_forward(move_value/10 + 8);
+
     enable_catapult();
-    // turn the little motor
+    
+    // turn the inner motor, the motor which perform the action of lifting the ball
     sg_motor(motor_lift,1000,-300);
+
     Sleep(3000);
+
     move_backward(move_value/10 + 8);
 }
 
-/*
-
-void get_ball_2(void){
-    int motor_lift = 66;
-    uint8_t sn;
-    if ( ev3_search_tacho_plugged_in(motor_lift,0, &sn, 0 )) {
-      set_tacho_position_sp(sn, -180);
-      set_tacho_speed_sp( sn, 300);
-      set_tacho_command_inx(sn, TACHO_RUN_TO_REL_POS);
-      Sleep(500);
-    }
-}
-*/
-
+// function to search the ball 
 void search_ball(void)
 {
     int i;
@@ -264,19 +224,24 @@ void search_ball(void)
     int prev_distance=10000;
     int threshold = 50;
     int flag_detected = 0;
+
     printf("SONAR found, reading sonar. It will print 36 values \n");
     if (ev3_search_sensor(LEGO_EV3_US, &sn_sonar,0)){
         if ( !get_sensor_value0(sn_sonar, &previous_value )) {
             previous_value = 0;
         }
+        // the robot will rotate and search for the ball accordingly to the protocol define below
         for(i=0; i < 36; i++)
         {
           turn_left(5);
           angle = i*5;
+          // get the value of the sonar sensor
           if ( !get_sensor_value0(sn_sonar, &current_value )) {
             current_value = 0;
           }
+
           printf( "\r%d : current value %f, previous value %f \n", angle, current_value,previous_value);
+          // the two followin condition express our way to detect the ball 
           if ((previous_value - current_value > threshold) && (previous_value < 500)) {
               printf("Ball detected\n");
               fflush( stdout );
@@ -301,82 +266,30 @@ void search_ball(void)
           previous_value = current_value;
           fflush( stdout );
         }
+        // if the ball is detected it perform a turn
         if (flag_detected){
+
             turn_left(6);
             turn_left(165);
+          
             get_ball(current_value);
+            
             turn_right(165);
         }
+
+        // get back to the initial angle
         while(i>1)
         {
           turn_right(5);
+
           printf("%d\n",i);
           fflush( stdout );
-
           i--;
         }
         turn_right(6);
+        // if the ball is detected then it throw the ball
         if (flag_detected){
             throw();
         }
     }
 }
-
-/*
-void search_ball_right(void)
-{
-    int i;
-    float current_value, previous_value;
-    uint8_t sn_sonar;
-    int angle;
-    int prev_distance=10000;
-    int threshold = 50;
-    int flag_detected = 0;
-    printf("SONAR found, reading sonar. It will print 36 values \n");
-    if (ev3_search_sensor(LEGO_EV3_US, &sn_sonar,0)){
-        if ( !get_sensor_value0(sn_sonar, &previous_value )) {
-            previous_value = 0;
-        }
-        for(i=0; i < 36; i++)
-        {
-          turn_right(5);
-          angle = i*5;
-          if ( !get_sensor_value0(sn_sonar, &current_value )) {
-            current_value = 0;
-          }
-          printf( "\r%d : current value %f, previous value %f \n", angle, current_value,previous_value);
-          if ((previous_value - current_value > threshold) && (previous_value < 500)) {
-              printf("Ball detected\n");
-              fflush( stdout );
-              Sleep(5000);
-              break;
-              flag_detected = 1;
-          }
-          if ((current_value - previous_value > threshold) && (previous_value < 500)) {
-              printf("Ball missed but then detected\n");
-              fflush( stdout );
-              turn_left(5);
-              Sleep(5000);
-              flag_detected = 1;
-              break;
-          }
-
-          previous_value = current_value;
-          fflush( stdout );
-        }
-        if (flag_detected){
-          turn_right(6);
-          turn_right(162);
-          get_ball(current_value);
-          turn_left(162);
-        }
-        while(i>0)
-        {
-          turn_left(5);
-          i--;
-        }
-        turn_left(5);
-        if (flag_detected){
-            throw();
-        }
-}*/
